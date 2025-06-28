cfe92903400055fa054e33aef1b6afaabbd5d64b
braynerdev <dev.brayner@outlook.com>
2025-06-28 00:22:36 -0300

    first commit

---
+++ C#/ExercicoContratos/ExercicoContratos/Entities/Worker.cs
@@ -0,0 +1,57 @@
+﻿using ExercicoContratos.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoContratos.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Dep { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament dep)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Dep = dep;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nDepartaments: {Dep.Name}";
+        }
+
+        public void AddContract(HourContract Contract)
+        {
+            Contracts.Add(Contract);
+        }
+        public void RemoveContract(HourContract Contract)
+        {
+            Contracts.Remove(Contract);
+        }
+        public Double Income(int year, int mouth)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract Contract in Contracts)
+            {
+                if (Contract.Date.Year == year && Contract.Date.Month == mouth)
+                {
+                    sum += Contract.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ C#/ExercicoContratos/ExercicoContratos/Program.cs
@@ -0,0 +1,64 @@
+﻿using ExercicoContratos.Entities.Enum;
+using ExercicoContratos.Entities;
+using System;
+using System.Globalization;
+
+namespace ExercicoContrato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter departament's name: ");
+            string NameDepartaments = Console.ReadLine();
+
+            Departament DepartamentWorker = new Departament(NameDepartaments); 
+
+
+
+            Console.WriteLine("Enter worker Date: ");
+
+            Console.Write("Name: ");
+            string NameWorker = Console.ReadLine();
+
+            Console.Write("Level(Junior/MidLevel/Senior): ");
+            WorkerLevel Level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.Write("Base Salary: ");
+            double Salary  = double.Parse(Console.ReadLine());
+
+            Worker WorkeRegister = new Worker(NameWorker, Level, Salary, DepartamentWorker);
+
+
+            Console.Write("How many contracts to this worker? ");
+            int AmountContract = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < AmountContract; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract Date: ");
+
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime Date = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy",CultureInfo.InvariantCulture);
+
+                Console.Write("Value per hour: ");
+                double ValuerHour = double.Parse(Console.ReadLine());
+
+                Console.Write("Duraction (hours): ");
+                int Duraction = int.Parse(Console.ReadLine());
+
+                HourContract Contract = new HourContract(Date,ValuerHour,Duraction);
+                WorkeRegister.AddContract(Contract);
+            }
+
+            Console.Write("Enter mouth and to calculate income (MM/YYYY): ");
+            string MouthAndYear = Console.ReadLine();
+            string[] SplitMouthAndYear = MouthAndYear.Split('/');
+            int Mouth = int.Parse(SplitMouthAndYear[0]);
+            int Year = int.Parse(SplitMouthAndYear[1]);
+            Console.WriteLine(WorkeRegister.ToString());
+            double ValueTotalGain = WorkeRegister.Income(Year,Mouth);
+
+            Console.Write($"Income for {MouthAndYear}: {ValueTotalGain.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
